26b90c1d3e31fa1db8f5f2061715231e70016d1f
nuthanss <nuthanss1996@gmail.com>
2020-07-21 03:00:48 +0530

    check

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express()
 
 var port = process.env.PORT || 3000
 
-// Define paths for Express configuratuon
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
